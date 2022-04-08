bef1093f9e15b51d0df12fcfc1613cc079c976ac
Romen, Ter Gazarian <romen.tergazarian@gmail.com>
2022-04-08 11:18:37 -0300

    Subiendo practica React.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {TodoCounter} from "./TodoCounter";
+import { TodoSearch } from "./TodoSearch";
+import { TodoItem } from "./TodoItem";
+import { CreateTodoButton } from "./CreateTodoButton";
+import { TodoList } from "./TodoList";
+
+//import './App.css';
+
+const todos = [
+  {text:'Cortar cebolla', complete:'false'},
+  {text:'Cursar react', complete:'false'},
+  {text:'Llorar :(', complete:'false'}
+
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <TodoCounter />
+    <TodoSearch />
+
+    <TodoList>
+      {todos.map(todo =>(
+        <TodoItem key={todo.text} text={todo.text} />
+      ))}
+    </TodoList>
+
+    <CreateTodoButton />
+    </>
+);
 }
 
 export default App;
